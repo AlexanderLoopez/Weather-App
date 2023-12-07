7faef63e1d0aeb02324531a8ba84384bf1612160
Alexander <alexanderls212@gmail.com>
2023-12-06 23:07:28 -0600

    Updating skeleton

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+//module.exports = {
+   // key: '{ 7c3e05e851f1dfec62f00f4f999fcf0b }',
+   // base: 'https://api.openweathermap.org/data/2.5/' ,
+   // icon: 'https://openweathermap.org/img/w',
+// }; 
+
