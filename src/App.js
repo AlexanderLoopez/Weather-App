a83198998be82b6477999aa2b8ea9542fa847f0a
Alexander <alexanderls212@gmail.com>
2023-12-12 18:30:21 -0600

    Working on API call

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Weather from './components/Weather.js';
-
-const api = {
-  key: '7c3e05e851f1dfec62f00f4f999fcf0b',
-  base: 'https://api.openweathermap.org/data/2.5'
-}
 
 function App() {
 
 const [lat, setLat] = useState([]);
 const [long, setLong] = useState([]);
-const [weatherData, setWeatherData] = useState([]);
+const [data, setData] = useState([]);
 
-useEffect(() => {
-  const fetchData = async () => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
+  useEffect(() =>{
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-  await fetch(`${api.base}/weather/?lat=${lat}&lon={long}&units=metric&APPID=${api.key}}`)
-  .then(res => res.json())
-  .then(result => {
-    setWeatherData(result)
-    console.log(result);
-  });
-}
-    fetchData();
-}, [lat, long])
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+  
+  }, [lat, long])
 
   return (
     <div className="App">
-      {(typeof weatherData.main != 'undefined') ? (
-        <Weather weatherData={weatherData}/>
-      ): (
-        <div>
-        </div>
-      )}
+      <header>
+        <h1>weather</h1>
+      </header>
     </div>
   );
 };
