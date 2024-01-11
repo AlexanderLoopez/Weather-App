b2bd909efff0b1b2b32b7dc1cbd54e3abd657041
Alexander <alexanderls212@gmail.com>
2024-01-10 23:59:19 -0600

    Working part 2

--- src/components/currentLocation.js
+++ src/components/currentLocation.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import ReactAnimatedWeather from 'react-animated-weather';
+import React, { useState, useEffect } from 'react';
+// import ReactAnimatedWeather from 'react-animated-weather';
 
+const CurrentLocation = ({ getWeatherData }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const getCurrentPosition = () => {
+      setLoading(true);
 
-class Weather extends React.Component {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          setLoading(true);
+          
+          navigator.geolocation.getCurrentPosition(
+            async (position) => {
+              const { latitude, longitude } = position.coords;
 
-  componentDidMount() {
-    if (navigator.geolocation) {
-      this.getPosition()
-        //If user allow location
-        .then((position) => {
-          this.getWeather(position.coords.latitud, position.coords.longitude);
-          })
-  
-        .catch((err) => {
-          //If user denies location
-          this.getWeather(28.67, 77.22);
-          alert(
-            'You have disabled location service. Allow "This App" to access your location. Your current location will be used for calculating real time weather.'
+              try {
+                const apiKey = '7c3e05e851f1dfec62f00f4f999fcf0b';
+                const response = await fetch(
+                  'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}'
+                );
+                const data = await response.json();
+
+                getWeatherData(data.name);
+              } catch (error) {
+                setError('Error getting weather data based on current location.');
+              } finally {
+                setLoading(false);
+              }
+            },
+            (error) => {
+              setError('Error getting current location.');
+              setLoading(false);
+            }
           );
-        });
-  
-      } else {
-          alert('Geolocation not avaible');
-      }
-      
-      this.timerID = setInterval(
-        () => this.getWeather(this.state.lat, this.state.lon),
-        700000
-      );
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.timerID);
-    }
-};
+        };
 
+        getCurrentLocation();
+      ), [getWeatherData]);
+
+      return (
+        <div>
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+        </div>
+      );
+    };
+
+export default CurrentLocation;
